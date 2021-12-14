439a544ad4f580145eda49b2996271aa38ba440d
dantofema <dantofema@gmail.com>
2021-12-14 11:46:50 -0300

    refactoring - modelView & collectionView

--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -1,4 +1,7 @@
-<div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg bg-white p-5 space-y-5">
+<div
+        {{ $attributes->merge(['class'=>"align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg bg-white
+    space-y-5"]) }}
+>
     {{ $slot }}
 </div>
 
---
+++ resources/views/components/root.blade.php
@@ -0,0 +1,5 @@
+<div
+        {{ $attributes->merge(['class'=>"space-y-5"]) }}
+>
+    {{ $slot }}
+</div>
